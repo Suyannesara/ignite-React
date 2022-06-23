fa753e9ec8216082719c683f0f86790a3072daae
Suyanne Miranda <66568437+Suyannesara@users.noreply.github.com>
2022-06-23 20:26:13 -0300

    Initializing with graphql and making queries using apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
+import { gql, useQuery } from "@apollo/client"
+import { useEffect } from "react"
+import { client } from "./lib/apolo"
+
+const GET_LESSONS_QUERY = gql `
+  query {
+    lessons {
+      id
+      title
+
+    }
+  }
+
+`
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
 function App() {
 
+  const { data } = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY)
+  console.log(data)
+
+
+  // //How to make a query on apollo client
+  // useEffect(() => {
+  //   client.query({
+  //     query: GET_LESSONS_QUERY
+  //   }).then(response => {
+  //     console.log(response.data)
+  //   })
+
+  // }, [])
+
   return (
-    <h1 className="text-2xl text-violet-500">oiiii</h1>
+    <ul>
+      { data?.lessons.map (lesson => {
+        return <li key={ lesson.id }>{ lesson.title }</li>
+      })}
+    </ul>
   )
 
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+import { ApolloProvider } from '@apollo/client'
 import './styles/global.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { client } from './lib/apolo'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ApolloProvider client={client} >
     <App />
+    </ApolloProvider>
   </React.StrictMode>
 )
