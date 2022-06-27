76d6beb89808ac19cf6de0d784393840e4c6af12
Suyanne Miranda <66568437+Suyannesara@users.noreply.github.com>
2022-06-26 21:34:08 -0300

    Saving subscribers on graphQL API

--- src/lib/apolo.ts
+++ src/lib/apolo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4qb4snd3xj401w7f4oc7isf/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
